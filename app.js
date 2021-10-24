33d4244b69543b8478830b231210f295f574aad7
Adrian Sian <adriansian@gmail.com>
2021-10-24 15:56:54 +0800

    backend: update the node-modules
    Change-Id: Ia140ee68a67ebc03785a0d904ba5f9392f70faa9

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { google } = require('googleapis');
 const app = express();
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
