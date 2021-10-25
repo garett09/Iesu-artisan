69d405e4bd0ca2913ba08b3abb6bfe10f0fe7e6a
Adrian Sian <adriansian@gmail.com>
2021-10-26 00:17:45 +0800

    formatting: format the backend code
    Change-Id: I12965301b6e4ab3c48076fb11ac8cea4c3651d58

--- app.js
+++ app.js
@@ -1,72 +1,70 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
-/*
-app.use(express.static(__dirname + '/'));
-app.use(express.urlencoded({ extended: true}));
 
-app.get("/", (req,res) => {
-    res.render("index")
-}
-*/
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
+  res.render("index");
+});
 app.get("/survey", (req, res) => {
-    res.render("survey.ejs")
+  res.render("survey");
 });
 
 app.get("/verification", (req, res) => {
-    res.render("verification.ejs")
+  res.render("verification");
 });
+
+
+
+//survey backennd
 app.post("/survey", async (req, res) => {
-    const { likert, likert1, likert2 } = req.body;
+  const { likert, likert1, likert2 } = req.body;
 
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
-    //create client instance for auth
-    const client = await auth.getClient();
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets"
+  });
+  // create client instance for auth
+  const client = await auth.getClient();
 
-    // instance of google sheets api
+  // instance of google sheets api
 
-    const googleSheets = google.sheets({ version: "v4", auth: client });
+  const googleSheets = google.sheets({ version: "v4", auth: client });
 
-    const spreadsheetId = "1HrUAO8EfE4TanTL1vcaV-HCmaiKLvQZmE6yhDaQn8Rk"
+  const spreadsheetId = "1HrUAO8EfE4TanTL1vcaV-HCmaiKLvQZmE6yhDaQn8Rk";
 
-    // get metadata about spreadsheet
-    const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId,
-    });
+  // get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId
+  });
 
-    // Read rows from spreadsheet
+  // Read rows from spreadsheet
 
-    const getRows = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: "Sheet1!A:C"
-    });;
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:C"
+  });
 
-    // Write row(s) to spreadsheet
+  // Write row(s) to spreadsheet
 
-    await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "Sheet1!A:C",
-        valueInputOption: "USER_ENTERED",
-        resource: {
-            values: [[likert, likert1, likert2]],
-        },
-    });
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:C",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[likert, likert1, likert2]]
+    }
+  });
 
-    res.send(res.render("verification"))
-})
+  res.send(res.render("verification"));
+});
 
-app.listen(PORT, (req, res) => console.log("Running on port " + PORT))
+app.listen(PORT, (req, res) => console.log("Running on port " + PORT));
