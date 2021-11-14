cbd9d88da43c81ec0af63653cf9c0ade5b966ab6
Aaron Mayor <aaronkarl.mayor.iics@ust.edu.ph>
2021-11-14 22:45:30 +0800

    frontend: added search and filter for database table
    Change-Id: I1e64a33a45d79b9e3f692bde42b79bbcb49e6cd6

--- public/js/script.js
+++ public/js/script.js
@@ -31,6 +31,32 @@ $(document).ready(function () {
         });
     });
 
+     // Setup - add a text input to each footer cell
+     $('#example tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+ 
+    // DataTable
+    var table = $('#example').DataTable({
+        initComplete: function () {
+            // Apply the search
+            this.api().columns().every( function () {
+                var that = this;
+ 
+                $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                    if ( that.search() !== this.value ) {
+                        that
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
+        }
+    });
+ 
+
+
     // team carousel 
     $('.team .owl-carousel').owlCarousel({
         loop: true,
@@ -80,3 +106,4 @@ $(document).ready(function () {
     });
 
 });
+
