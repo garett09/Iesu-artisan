0569f59784e111abacc062da1f81d9c9390ac605
Aaron Mayor <aaronkarl.mayor.iics@ust.edu.ph>
2021-11-15 09:41:47 +0800

    frontend: fixed tabel design
    Change-Id: I8222c700e3e6654c84953f0b2829251b08aa3e47

--- public/js/script.js
+++ public/js/script.js
@@ -32,29 +32,13 @@ $(document).ready(function () {
     });
 
      // Setup - add a text input to each footer cell
-     $('#example tfoot th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+     $(document).ready(function() {
+        $('#example').DataTable( {
+            "dom": '<"top"fl>rt<"bottom"ip><"clear">',
+            "pagingType": "simple_numbers",
+            
+        } );
     } );
- 
-    // DataTable
-    var table = $('#example').DataTable({
-        initComplete: function () {
-            // Apply the search
-            this.api().columns().every( function () {
-                var that = this;
- 
-                $( 'input', this.footer() ).on( 'keyup change clear', function () {
-                    if ( that.search() !== this.value ) {
-                        that
-                            .search( this.value )
-                            .draw();
-                    }
-                } );
-            } );
-        }
-    });
- 
 
 
     // team carousel 
